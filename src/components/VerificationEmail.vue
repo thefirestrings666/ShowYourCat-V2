a647417dfa5ee212eab3d524cca814385ce4d800
Sidney Betous <sidney@MBP-de-Sidney.fritz.box>
2020-05-17 21:57:59 +0200

    pull

--- src/components/VerificationEmail.vue
+++ src/components/VerificationEmail.vue
@@ -47,6 +47,9 @@ export default {
 
           // ...
         })
+        .then(() => {
+          firebase.currentUser().reload()
+        })
     },
 
     ...mapMutations('authentication', ['setUser']),
